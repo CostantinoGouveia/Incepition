61fe0cf7c6f6ea8a5ebcb71c36d51c1b110d77f7
Costantino Gouveia <costantino.gouveia938@gmail.com>
2026-04-16 17:19:52 +0100

    implementei o servico nginx

---
+++ srcs/requirements/nginx/tools/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+mkdir -p /etc/nginx/ssl
+
+if [ ! -f "/etc/nginx/ssl/${DOMAIN_NAME}.crt" ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout "/etc/nginx/ssl/${DOMAIN_NAME}.key" \
+        -out "/etc/nginx/ssl/${DOMAIN_NAME}.crt" \
+        -subj "/C=AO/ST=Luanda/L=Luanda/O=42/CN=${DOMAIN_NAME}"
+fi
+
+envsubst '${DOMAIN_NAME}' \
+    < /etc/nginx/templates/nginx.conf.template \
+    > /etc/nginx/conf.d/default.conf
+
+exec nginx -g "daemon off;"
